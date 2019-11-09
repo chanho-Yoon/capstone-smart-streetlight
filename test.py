fa8ccc8fd57c18b6583f38a33f003e8d0ee54d6f
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-11-09 21:07:28 +0900

    flask test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import RPi. GPIO as GPIO
 from time import sleep
 import time
 import serial
-ser = serial.Serial('/dev/ttyACM0',9600);
+ser = serial.Serial('/dev/ttyACM0',9600)
 
 
 '''servo moter sensor'''
@@ -17,17 +17,17 @@ duty = float(angle) / 10.0 + 2.5
 p.ChangeDutyCycle(duty)
 time.sleep(1)
 '''sound left, right'''
-soundL=0;
-soundR=0;
+soundL=0
+soundR=0
 	
 while 1 :
-	dustVal = ser.readline();
-	print dustVal;
+	dustVal = ser.readline()
+	print dustVal
 	
 	value = dustVal.split();	
 	
-	soundL = int(value[2]);
-	soundR = int(value[3]);
+	soundL = int(value[2])
+	soundR = int(value[3])
 	
 	if soundL > 140 :
 		angle = 40
@@ -38,7 +38,7 @@ while 1 :
 		angle = 90 
                 duty = float(angle) / 10.0 + 2.5
                 p.ChangeDutyCycle(duty)
-		soundR = 0;
+		soundR = 0
 
 	elif soundR > 140 :
 		angle = 150
@@ -49,4 +49,4 @@ while 1 :
 		angle = 90 
                 duty = float(angle) / 10.0 + 2.5
                 p.ChangeDutyCycle(duty)
-		soundL = 0;
+		soundL = 0

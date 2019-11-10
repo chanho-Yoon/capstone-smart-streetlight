b8f902398e15cb372a9a0cf8852c49f6a5c88fff
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-11-10 20:15:30 +0900

    DB Insert Test

--- test_ip.py
+++ test_ip.py
@@ -6,6 +6,7 @@ import pymysql
 ser = serial.Serial('/dev/ttyACM0',9600)
 db = pymysql.connect(host="localhost",user="root",passwd="1234",db="kdn",charset='utf8')
 curs=db.cursor()
+
  
 while 1:
         sonic = ser.readline()

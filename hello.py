62c8d43b22e08c48ba523d2ace8ef783f54bc3f3
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-11-12 21:24:51 +0900

    flask 프레임워크 사용

--- hello.py
+++ hello.py
@@ -4,7 +4,10 @@ import datetime
 from time import sleep
 import time
 import serial
-ser = serial.Serial('/dev/ttyACM0', 9600)
+import MySQLdb
+import pymysql
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
 
 #servo moter sensor
 GPIO.setmode(GPIO.BCM)
@@ -12,11 +15,12 @@ GPIO.setup(26, GPIO.OUT)
 
 p = GPIO.PWM(26, 100)
 p.start(5)
+
 #servo moter 90 reset
-angle = 90
-duty = float(angle) / 10.0 + 2.5
-p.ChangeDutyCycle(duty)
-time.sleep(1)
+#angle = 90
+#duty = float(angle) / 10.0 + 2.5
+#p.ChangeDutyCycle(duty)
+#time.sleep(1)
 #sound left, right
 soundL = 0
 soundR = 0
@@ -24,44 +28,11 @@ soundR = 0
 app = Flask(__name__)
 
 @app.route("/")
-
-#def hello():
-#	now = datetime.datetime.now()
-#	timeString = now.strftime("%Y-%m-%d %H:%M:%S")
-#	templateData = {
-#		'title' : 'Raspberry Pi 3',
-#		'time' : timeString
-#	}
-#	return render_template('index.html' , **templateData)
-
-
+@app.route("/main")
 def hello():
-    dustVal = ser.readline()
-    value = dustVal.split()
-
-    soundL = int(value[2])
-    soundR = int(value[3])
-    if soundL > 140:
-        angle = 40
-        duty = float(angle) / 10.0 + 2.5
-        p.ChangeDutyCycle(duty)
-        print "40-----"
-        time.sleep(6)
-        angle = 90
-        duty = float(angle) / 10.0 + 2.5
-        p.ChangeDutyCycle(duty)
-        soundR = 0
-
-    elif soundR > 140:
-        angle = 150
-        duty = float(angle) / 10.0 + 2.5
-        p.ChangeDutyCycle(duty)
-        print "150----"
-        time.sleep(6)
-        angle = 90
-        duty = float(angle) / 10.0 + 2.5
-        p.ChangeDutyCycle(duty)
-        soundL = 0
+    dataVal = ser.readline()
+    print dataVal
+    value = dataVal.split()
 
     lightString = value[0]
     shockString = value[1]
@@ -70,14 +41,103 @@ def hello():
     dustString = value[4]
 
     templateData = {
-        'light' : lightString,
-		'shock' : shockString,
-		'soundL' : soundLString,
-		'soundR' : soundRSTring,
-		'dust' : dustString
-	}
-	
-    return render_template('index.html',**templateData)
+        'light': lightString,
+      		'shock': shockString,
+      		'soundL': soundLString,
+      		'soundR': soundRSTring,
+      		'dust': dustString
+    }
+    return render_template('main.html', **templateData)
+#cctv
+@app.route("/cctv.html")
+def cctv():
+    dataVal = ser.readline()
+    print dataVal
+    value = dataVal.split()
+
+    lightString = value[0]
+    shockString = value[1]
+    soundLString = value[2]
+    soundRSTring = value[3]
+    dustString = value[4]
+
+    templateData = {
+        'light': lightString,
+      		'shock': shockString,
+      		'soundL': soundLString,
+      		'soundR': soundRSTring,
+      		'dust': dustString
+    }
+
+    return render_template('cctv.html', **templateData)
+#shock
+@app.route("/shock.html")
+def shock():
+    dataVal = ser.readline()
+    print dataVal
+    value = dataVal.split()
+
+    lightString = value[0]
+    shockString = value[1]
+    soundLString = value[2]
+    soundRSTring = value[3]
+    dustString = value[4]
+
+    templateData = {
+        'light': lightString,
+      		'shock': shockString,
+      		'soundL': soundLString,
+      		'soundR': soundRSTring,
+      		'dust': dustString
+    }
+
+    return render_template('shock.html', **templateData)
+    
+#dust
+@app.route("/dust.html")
+def dust():
+    dataVal = ser.readline()
+    print dataVal
+    value = dataVal.split()
+
+    lightString = value[0]
+    shockString = value[1]
+    soundLString = value[2]
+    soundRSTring = value[3]
+    dustString = value[4]
+
+    templateData = {
+        'light': lightString,
+      		'shock': shockString,
+      		'soundL': soundLString,
+      		'soundR': soundRSTring,
+      		'dust': dustString
+    }
+
+    return render_template('dust.html', **templateData)
+
+#led
+@app.route("/light.html")
+def light():
+    dataVal = ser.readline()
+    print dataVal
+    value = dataVal.split()
+
+    lightString = value[0]
+    shockString = value[1]
+    soundLString = value[2]
+    soundRSTring = value[3]
+    dustString = value[4]
+
+    templateData = {
+        'light': lightString,
+      		'shock': shockString,
+      		'soundL': soundLString,
+      		'soundR': soundRSTring,
+      		'dust': dustString
+    }
+
+    return render_template('light.html', **templateData)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)

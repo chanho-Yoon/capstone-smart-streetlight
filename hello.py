696b1f88b041ba803ab62824cd209438e112d7c1
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-11-14 18:57:38 +0900

    기존에 분리되어 있던 웹들을 메인페이지에서 다 보여주기 위해서 쓸모 없는 곳 다 지움

--- hello.py
+++ hello.py
@@ -10,12 +10,13 @@ import pymysql
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
 
 #servo moter sensor
+'''
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT)
 
 p = GPIO.PWM(26, 100)
-p.start(5)
-
+p.start(3)
+'''
 #servo moter 90 reset
 #angle = 90
 #duty = float(angle) / 10.0 + 2.5
@@ -26,7 +27,6 @@ soundL = 0
 soundR = 0
 
 app = Flask(__name__)
-
 @app.route("/")
 @app.route("/main")
 def hello():
@@ -36,108 +36,23 @@ def hello():
 
     lightString = value[0]
     shockString = value[1]
-    soundLString = value[2]
-    soundRSTring = value[3]
-    dustString = value[4]
+    soundString = value[2]
+    dustString = value[3]
 
     templateData = {
         'light': lightString,
-      		'shock': shockString,
-      		'soundL': soundLString,
-      		'soundR': soundRSTring,
-      		'dust': dustString
+      	'shock': shockString,
+      	'sound': soundString,
+      	'dust': dustString
     }
+
     return render_template('main.html', **templateData)
 #cctv
 @app.route("/cctv.html")
 def cctv():
-    dataVal = ser.readline()
-    print dataVal
-    value = dataVal.split()
 
-    lightString = value[0]
-    shockString = value[1]
-    soundLString = value[2]
-    soundRSTring = value[3]
-    dustString = value[4]
+    return render_template('cctv.html')
 
-    templateData = {
-        'light': lightString,
-      		'shock': shockString,
-      		'soundL': soundLString,
-      		'soundR': soundRSTring,
-      		'dust': dustString
-    }
-
-    return render_template('cctv.html', **templateData)
-#shock
-@app.route("/shock.html")
-def shock():
-    dataVal = ser.readline()
-    print dataVal
-    value = dataVal.split()
-
-    lightString = value[0]
-    shockString = value[1]
-    soundLString = value[2]
-    soundRSTring = value[3]
-    dustString = value[4]
-
-    templateData = {
-        'light': lightString,
-      		'shock': shockString,
-      		'soundL': soundLString,
-      		'soundR': soundRSTring,
-      		'dust': dustString
-    }
-
-    return render_template('shock.html', **templateData)
-    
-#dust
-@app.route("/dust.html")
-def dust():
-    dataVal = ser.readline()
-    print dataVal
-    value = dataVal.split()
-
-    lightString = value[0]
-    shockString = value[1]
-    soundLString = value[2]
-    soundRSTring = value[3]
-    dustString = value[4]
-
-    templateData = {
-        'light': lightString,
-      		'shock': shockString,
-      		'soundL': soundLString,
-      		'soundR': soundRSTring,
-      		'dust': dustString
-    }
-
-    return render_template('dust.html', **templateData)
-
-#led
-@app.route("/light.html")
-def light():
-    dataVal = ser.readline()
-    print dataVal
-    value = dataVal.split()
-
-    lightString = value[0]
-    shockString = value[1]
-    soundLString = value[2]
-    soundRSTring = value[3]
-    dustString = value[4]
-
-    templateData = {
-        'light': lightString,
-      		'shock': shockString,
-      		'soundL': soundLString,
-      		'soundR': soundRSTring,
-      		'dust': dustString
-    }
-
-    return render_template('light.html', **templateData)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)

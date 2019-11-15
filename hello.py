6eb1072389989dc2fabeac617f87e68c1d13345b
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-11-15 19:35:52 +0900

    라즈베리와 아두이노 직렬포트 사이에 무슨 문제인지 데이터 받다가 깨져서 들어옴

--- hello.py
+++ hello.py
@@ -4,19 +4,11 @@ import datetime
 from time import sleep
 import time
 import serial
-import MySQLdb
-import pymysql
+#import pymysql
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=None)
 
 #servo moter sensor
-'''
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.OUT)
-
-p = GPIO.PWM(26, 100)
-p.start(3)
-'''
 #servo moter 90 reset
 #angle = 90
 #duty = float(angle) / 10.0 + 2.5
@@ -30,23 +22,64 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/main")
 def hello():
+    '''
+    try:
+        dataVal = ser.read(ser.inWaiting())
+        print dataVal 
+        value = dataVal.split(' ')         
+        lightString = value[0]
+        shockString = value[1]
+        soundString = value[2]
+        dustString = value[3]                         
+        
+    except (OSError, serial.serialutil.SerialException):
+        lightString = '1'
+        shockString = '2'
+        soundString = '3'
+        dustString = '4'
+    '''
+    
     dataVal = ser.readline()
-    print dataVal
-    value = dataVal.split()
-
+    print (dataVal)
+    value = dataVal.split(' ')         
     lightString = value[0]
     shockString = value[1]
     soundString = value[2]
-    dustString = value[3]
+    dustString = value[3]                         
 
     templateData = {
-        'light': lightString,
-      	'shock': shockString,
-      	'sound': soundString,
-      	'dust': dustString
+        'light':lightString,
+        'shock':shockString,
+        'sound':soundString,
+        'dust':dustString
     }
-
-    return render_template('main.html', **templateData)
+    
+    return render_template('main.html',**templateData)
+    
+    
+    '''
+    if ser.readable():
+    dataVal = ser.readline()
+    print dataVal 
+                 
+        value = dataVal.split(' ')         
+        lightString = value[0]
+        shockString = value[1]
+        soundString = value[2]
+        dustString = value[3]
+    
+    else :
+        lightString = '1'
+        shockString = '2'
+        soundString = '3'
+        dustString = '4'
+    
+    return render_template('main.html',
+                           light=lightString,
+                           shock=shockString,
+                           sound=soundString,
+                           dust=dustString)
+    '''
 #cctv
 @app.route("/cctv.html")
 def cctv():
@@ -55,4 +88,4 @@ def cctv():
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
